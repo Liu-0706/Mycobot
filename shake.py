766fa6a3333537f21acfcb75fbbf85c69ff743f0
wuchangji <changji.wu@elephantrobotics.com>
2025-05-21 20:13:41 +0800

    Add files manually copied into repo

--- shake.py
+++ shake.py
@@ -7,6 +7,7 @@ from head import save
 from head import initialize
 from head import add_jitter
 from head import random_end
+from head import predict_angles
 
 mc = MyCobot('/dev/ttyAMA0', 1000000)
 
@@ -15,24 +16,44 @@ initialize()
 start_coords = [120, 20, 140, 0, 180, 180]
 #end_coords = [200, 20, 140, 0, 180, 180]
 
+end_coords = random_end()
+print("end_coords",end_coords)
+end_coords = add_jitter(end_coords)
+print("end_coords_jitter",end_coords)
+
 def shake(start_coords, end_coords):
-    jitter_coodrs = add_jitter(end_coords)
-
-    mc.send_coords(start_coords, 50, 1)
+    start_coords[2] += 30
+    mc.send_coords(start_coords, 40, 1)
     time.sleep(2)
-    mc.send_coords(jitter_coodrs, 20, 1)
+    start_coords[2] -= 30
+    mc.send_coords(start_coords, 20, 1)
+    time.sleep(1)
+
+
+    mc.send_coords(end_coords, 20, 1)
+    time.sleep(2)
+    end_coords[2] += 30
+    mc.send_coords(end_coords, 20, 1)
+    time.sleep(1)
+
+
+def shake_with_gpr(start_coords, end_coords):
+    end_angles = predict_angles(end_coords[:3])
+    print("end_angles",end_angles)
+    start_coords[2] += 30
+    mc.send_coords(start_coords, 40, 1)
+    time.sleep(2)
+    start_coords[2] -= 30
+    mc.send_coords(start_coords, 20, 1)
+    time.sleep(1)
+
+    mc.send_angles(end_angles, 20)
     time.sleep(2)
 
-    actual_end_coords = mc.get_coords()
-    angles = mc.get_angles()
+shake(start_coords, end_coords)
 
-    save(end_coords, actual_end_coords, angles)
+shake_with_gpr(start_coords, end_coords)
 
-for _ in range(10):
-    end_coords = random_end()
-    print("end_coords",end_coords)
-    for _ in range(20):
-        shake(start_coords, end_coords)
 print("end shake.py")
 close()
 
