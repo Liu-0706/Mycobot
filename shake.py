6740bc24539228cc284849ef40f7d57b5e0e8b12
Liu-0706 <liuzhuo0706@outlook.com>
2025-05-26 15:51:18 +0200

    Add files manually copied into repo

--- shake.py
+++ shake.py
@@ -24,27 +24,40 @@ mc.send_coords(start_coords, 20, 1)
 time.sleep(2)
 
 # 动态绘制并补偿
-steps = 30
+steps = 100
+prev_angles = mc.get_angles()
+alpha = 0.7  # 平滑系数
+
 for i in range(steps):
     ratio = i / steps
     target = head.linear_interp(start_coords[:3], end_coords[:3], ratio)
     actual = mc.get_coords()
     error = [a - t for a, t in zip(actual[:3], target)]
-    
-    features = np.array(target + error).reshape(1, -1)
-    angles = [model.predict(features)[0] for model in gpr_models]
 
-    mc.send_angles(angles, 50)
-    time.sleep(0.1)
+    features = np.array(target + error).reshape(1, -1)
+    raw_angles = [model.predict(features)[0] for model in gpr_models]
+
+    # 平滑处理角度输出
+    smoothed_angles = [
+        alpha * new + (1 - alpha) * old
+        for new, old in zip(raw_angles, prev_angles)
+    ]
+
+    mc.send_angles(smoothed_angles, 40)
+    prev_angles = smoothed_angles
+
+    # 动态等待直到基本到位
+    while not mc.is_in_position(smoothed_angles, 2):
+        time.sleep(0.01)
 
 # 提笔动作
 final = end_coords[:]
 final[2] += 30
 mc.send_coords(final, 20, 1)
-time.sleep(2)
+while not mc.is_in_position(final, 2):
+    time.sleep(0.01)
 
 head.close()
-
 
 
 """
